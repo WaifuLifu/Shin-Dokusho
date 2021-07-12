dc06379cbffd594f4d91c438b42a9eb3264e157a
ritikverma098 <ritikverma0986@gmail.com>
2021-07-12 23:16:49 +0530

    First Commit (12-07-2021)(23:16:23)

---
+++ app/src/main/java/com/waifu_laifu/shin_dokusho/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.waifu_laifu.shin_dokusho
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
